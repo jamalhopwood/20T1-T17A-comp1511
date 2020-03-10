40e59f66ac7f42f44cacf19c7b951abbe4316c93
jamalhopwood <jamal.hopwood@gmail.com>
2020-03-10 13:53:06 +0800

    wk3 notes too

--- tut03/error4.c
+++ tut03/error4.c
@@ -1,4 +1,11 @@
-int i = 0;
-while (i < 10)
-    printf("%d\n", i);
-    i = i + 1;
+#include <stdio.h>
+
+int main(void) {
+    int i = 0;
+    while (i < 10) {
+        printf("%d\n", i);
+        i = i + 1;
+    }
+
+    return 0;
+}
